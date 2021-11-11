911ebac816be4b5344d8ef217c9f41805b3ecaf5
Faizarabhi <faizarabhi8@gmail.com>
2021-11-11 17:04:09 +0100

    projet fini

---
+++ Qcm.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+	//e= essaye || r = nombre des qst i= les points j= le points gagnerr pour chaque qst
+	int r=1,a,e=1,j=0,i=0;
+	bool k= false;
+
+	printf("Bienvenu !\ns'il vous plais répendez au questions suivants\n");
+	
+	
+	
+	
+	//methode 1:
+	while(r<4){
+
+
+	switch(r){
+		case 1:
+			{ r++;
+			do{
+				printf("QST 1: 1+1 = \n 2 or 3\n");
+				scanf("%d",&a);
+				if(a==2){
+					i+=3;
+					k=true;
+					printf("true you  have %d points for this question \n",i);
+				
+				}
+				else{
+					printf ("false\n you have %d points\n",i);
+					--i;
+					e++;
+				}
+			}
+			while( e <= 3 && k== false );
+					e=1;
+					k = false;
+					break;
+			}
+			case 2:
+				{r++;
+					do{
+			
+				printf("QST 2: 2+1 = \n 2 or 3\n");
+				scanf("%d",&a);
+				if(a==3){
+					i+=3;
+					k=true;
+					printf("true you  have %d points for this question \n",i);}
+				else{
+						printf ("false\n you have %d points\n",i);
+						i--;
+						e++;
+					
+					}
+					}while(k==false && e<=3 );
+					e=1;
+					printf("%d",i);
+						k = false;
+					break;
+				}
+			case 3:{r++;
+					do{
+			
+				printf("QST 3: 6+7 = \n 0 or 13\n");
+				scanf("%d",&a);
+				if(a==13){
+					k=true;
+					i+=3;
+					printf("true you  have %d points for this question \n",i);}
+					else{
+						printf ("false\n you have %d points\n",i);
+						i--;
+						e++;
+					
+					}}while(k==false && e<=3 );
+					
+					printf("%d",i);
+					k = false;
+					break;
+					
+			}
+			printf("%d",i);
+			
+				
+	}}	
+ printf(" Votre score %d",i);
+	
+	
+}
